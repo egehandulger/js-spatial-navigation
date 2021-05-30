e3cd153efc987249e0ae7c740038d154812f0fb6
Egehan Dülger <14878259+egehandulger@users.noreply.github.com>
2021-05-30 21:43:02 +0200

    Create gpr package publishing action (#5)
    * Create npm-publish.yml
    
    * Indicate fork origin
    
    * Add scope to package name
    
    * Create .npmignore

--- spatial_navigation.js
+++ spatial_navigation.js
@@ -1,7 +1,7 @@
 /*
  * A javascript-based implementation of Spatial Navigation.
  *
- * Copyright (c) 2017 Luke Chang.
+ * Forked from
  * https://github.com/luke-chang/js-spatial-navigation
  *
  * Licensed under the MPL 2.0.
